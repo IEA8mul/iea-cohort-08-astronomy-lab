3eb1ad4c48645c86f04f3f50482f84c8eeec4f1c
IEA8mul <thomas.mulroy@libertymutual.com>
2022-10-18 20:08:49 -0400

    add to github

--- solarVS.py
+++ solarVS.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import requests
 import datetime
-ASTRONOMYAPI_ID="<adb8f84e-3dad-445b-acc5-92beddb1113a>"
-ASTRONOMYAPI_SECRET="<22e15557d1fd330bf114fd8948d33c8542c036f19041b97c166e43ba8cb775fea5aec3551f6ebee38d512f4b3c144c5645600cb96fed05af15d506d9c8717eaea42bdd103fc963fdbdc68e3b52bbd44b7bced65767ef7015d9de9a654318b93bf9b275f99f47b38d15c9ee67998f301c>"
+ASTRONOMYAPI_ID="adb8f84e-3dad-445b-acc5-92beddb1113a"
+ASTRONOMYAPI_SECRET="22e15557d1fd330bf114fd8948d33c8542c036f19041b97c166e43ba8cb775fea5aec3551f6ebee38d512f4b3c144c5645600cb96fed05af15d506d9c8717eaea42bdd103fc963fdbdc68e3b52bbd44b7bced65767ef7015d9de9a654318b93bf9b275f99f47b38d15c9ee67998f301c"
 def get_observer_location():
 #
 """Returns the longitude and latitude for the location of this machine.
